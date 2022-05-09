f75420f29f912d8c09ba7d21bce86a091e9badca
dannsgo <dannsgo@gmail.com>
2022-05-09 18:26:39 +0900

    Add: Style 완료, 리뷰 & 구매 링크 연결

--- src/components/MovieDetail/index.jsx
+++ src/components/MovieDetail/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import "./index.css";
 
 function MovieDetail() {
   const [movieData, setMovieData] = useState([]);
@@ -19,10 +20,46 @@ function MovieDetail() {
     <>
       {movie ? (
         <>
-          <img src={movie.image} alt=""></img>
-          <div>{movie.title.replace(/[</b>]/gi, "")}</div>
-          <div>{movie.subtitle.replace(/[</b>]/gi, "")}</div>
-          <div>{movie.director.replace(/[|]/gi, "")}</div>
+          <div className="movie_detail">
+            <div className="detail_area">
+              <div className="detail_box">
+                <img src={movie.image} alt="" />
+                <div className="detail_info">
+                  <div className="info_title">
+                    {movie.title.replace(/[</b>]/gi, "")}
+                  </div>
+                  <div className="info_subtitle">
+                    {movie.subtitle.replace(/[</b>]/gi, "")}
+                  </div>
+                  <div className="info_director">
+                    {movie.director.replace(/[|]/gi, "")}
+                  </div>
+                  <div className="info_actor">
+                    {movie.actor.replace(/[|]/gi, ",\n")}
+                  </div>
+                  <div className="info_rating">
+                    <span className="material-icons-outlined">star</span>
+                    {movie.userRating}
+                  </div>
+                </div>
+              </div>
+              <div className="button_box">
+                <Link to="/review/posts">
+                  <button className="detail_review">
+                    리뷰<span className="material-icons-outlined">edit</span>
+                  </button>
+                </Link>
+                <Link to={`/movie/order/${movie.id}`}>
+                  <button className="detail_order">
+                    구매
+                    <span className="material-icons-outlined">
+                      shopping_bag
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </>
       ) : (
         <>X</>
